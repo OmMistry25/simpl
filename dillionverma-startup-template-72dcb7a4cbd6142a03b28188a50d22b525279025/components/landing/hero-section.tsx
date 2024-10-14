df1fb3c159ecae30787808cabd1f76ee20d071ec
Om Mistry <157780551+OmMistry25@users.noreply.github.com>
2024-10-14 05:39:51 +0000

    c14

--- dillionverma-startup-template-72dcb7a4cbd6142a03b28188a50d22b525279025/components/landing/hero-section.tsx
+++ dillionverma-startup-template-72dcb7a4cbd6142a03b28188a50d22b525279025/components/landing/hero-section.tsx
@@ -34,7 +34,7 @@ export default function HeroSection() {
       </Button>
       <div
         ref={ref}
-        className="relative mt-[40rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+        className="relative mt-[80rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
       >
         <div
           className={`rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] ${
@@ -55,7 +55,7 @@ export default function HeroSection() {
             className="hidden relative w-full h-full rounded-[inherit] border object-contain dark:block"
           />
           <img
-            src="/hero-light4.png"
+            src="/hero-light5.png"
             alt="Hero Image"
             className="block relative w-full h-full  rounded-[inherit] border object-contain dark:hidden"
           />
