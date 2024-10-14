3f778bb9692eca8c5b11fde9040b81588ca1fe0c
Om Mistry <157780551+OmMistry25@users.noreply.github.com>
2024-10-14 16:05:40 +0000

    c23

--- website_code/components/landing/textreveal.tsx
+++ website_code/components/landing/textreveal.tsx
@@ -1,9 +1,11 @@
 import TextRevealByWord from "@components/magicui/text-reveal";
 
-export function TextRevealDemo() {
+export default function TextRevealDemo() {
   return (
     <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
       <TextRevealByWord text="Magic UI will change the way you design." />
     </div>
   );
 }
+
+
